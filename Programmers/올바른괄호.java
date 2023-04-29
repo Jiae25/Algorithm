758131eae8d9f91f15fb6187f61398140decd03d
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-04-30 01:51:16 +0900

    올바른 괄호 - Stack

--- Programmers/올바른괄호.java
+++ Programmers/올바른괄호.java
@@ -1,27 +1,30 @@
+import java.util.Stack;
+
 /*
 올바른 괄호
  */
 public class 올바른괄호 {
     static boolean solution(String s) {
         boolean answer = true;
-        int num = 0;
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '('){
-                num++;
-            }else{
-                num--;
-            }
-            if(num < 0){
+                stack.push('(');
+            }else if(stack.isEmpty()){
                 answer = false;
                 break;
+            }else {
+                stack.pop();
             }
         }
-        if(num > 0) answer = false;
+        if(!stack.isEmpty()) {
+            answer = false;
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        String s = "(()(";
+        String s = ")()(";
         System.out.println(solution(s));
     }
 }
