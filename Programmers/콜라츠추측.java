233942db80eeab306c1c6842105d390dce74a05d
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-05-03 23:25:16 +0900

    콜라츠 추측

---
+++ Programmers/콜라츠추측.java
@@ -0,0 +1,27 @@
+// 콜라츠 추측
+public class 콜라츠추측 {
+    public static int solution(int num) {
+        int answer = 0;
+        long n = num;
+        while(n != 1){
+            // 500번 이상 반복해도 1이 되지 않으면 -1 반환
+            if(answer >= 500){
+                return -1;
+            }
+
+            if(n%2 == 0){ // 입력된 수가 짝수라면 2로 나눔
+                n /= 2;
+            }else{ // 홀수인 경우
+                n = (n * 3) + 1;
+            }
+            answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 626331;
+        System.out.println(solution(n));
+
+    }
+}
