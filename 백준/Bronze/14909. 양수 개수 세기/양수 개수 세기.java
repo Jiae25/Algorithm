b1b935f456bf37165d2e57989677316d8d0bf2c2
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-08-19 23:50:49 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 520 ms, Memory: 105920 KB -BaekjoonHub

---
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int answer = 0;
+        for (int i = 0; i < input.length; i++){
+            if(Integer.parseInt(input[i]) > 0) answer++;
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
