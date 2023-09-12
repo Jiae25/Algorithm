7f1b73a78f55d1abb24eba2aed0fdb3d8f5f120e
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-09-12 23:51:30 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long count = 0;
+        if (n >= 3) {
+            count = (long) n * (n - 1) * (n - 2) / 6;
+        }
+        System.out.println(count);
+        System.out.println(3);
+    }
+}
