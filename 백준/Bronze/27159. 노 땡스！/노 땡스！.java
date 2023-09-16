7a1a966763dcbdaaab18ffe96f5f6c9dd933b425
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-09-16 23:59:02 +0900

    [Bronze III] Title: 노 땡스!, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/27159. 노 땡스！/노 땡스！.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int[] arr = new int[N];
+        int sum = 0;
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(s[i]);
+        sum += arr[0];
+        for (int i = 1; i < N; i++) {
+            if (arr[i-1] != arr[i]-1)
+                sum += arr[i];  // 이웃한 수가 1차이 나지 않는 경우 합하기
+        }
+        System.out.println(sum);
+    }
+}
