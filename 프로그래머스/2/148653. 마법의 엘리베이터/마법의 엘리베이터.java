083d6ceaf8bd6ac4d354ccce4ea7d9ad0824e521
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-11-22 22:14:33 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        while(storey > 0){
+            int num = storey % 10;
+            storey /= 10;
+            if(num > 5){ // 5 초과 시 더하기
+                answer += (10 - num);
+                storey++;
+            }else if(num < 5){ // 5 미만 시 빼기
+                answer += num;
+            }else if(num == 5){ // 5인 경우 앞자리 숫자에 따라서 변경
+                if(storey % 10 >= 5){
+                    answer += (10 - num);
+                    storey++;
+                }else{
+                    answer += num;
+                }
+            }
+        }
+        return answer;
+    }
+}
