6f70302b36a08e8e63581439e8411fbe165e2ca2
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2023-12-09 03:12:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1408 ms, Memory: 111120 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++){
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(numbers);
+        StringBuilder sb = new StringBuilder();
+        for (int num : numbers){
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb.delete(sb.length() - 1, sb.length()));
+
+    }
+}
