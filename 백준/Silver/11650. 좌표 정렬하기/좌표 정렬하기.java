6c2522c43971935250d2a1fa66d3a9d4937cf46a
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-01-08 02:12:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 816 ms, Memory: 56024 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -11,26 +11,18 @@ public class Main {
         int[][] arr = new int[N][2];
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr[i][0] = x;
-            arr[i][1] = y;
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr, new Comparator<int[]>(){
             @Override
             public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[0], o2[0]); // x 좌표 비교
-            }
-        });
-
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
                 if(o1[0] == o2[0]){
                     return Integer.compare(o1[1], o2[1]);
+                }else {
+                    return Integer.compare(o1[0], o2[0]); // x 좌표 비교
                 }
-                return 0;
             }
         });
 
