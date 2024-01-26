4f3221e48cb743a39f2526ba091561575c104650
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-01-26 18:57:19 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,27 +4,30 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        Stack<Character> stack = new Stack<>();
-        for (int t = 0; t < T; t++){
-            stack.clear();
+        int T = Integer.parseInt(br.readLine()); // TC
+        for(int t = 0; t < T; t++){
+            Stack<Character> stack = new Stack<>();
             String input = br.readLine();
-            if(input.length() % 2 != 0 || input.charAt(0) == ')') {
+
+            if(input.length() % 2 != 0 || input.charAt(0) == ')'){ // 짝이 맞지 않는 괄호 || )로 시작되는 괄호
                 System.out.println("NO");
                 continue;
             }
-            for (int i = 0; i < input.length(); i++){
-                if(stack.isEmpty() || input.charAt(i) == '(') {
-                    stack.push(input.charAt(i));
-                }else if(stack.peek() == '(' && input.charAt(i) == ')'){
+
+            for(int i = 0; i < input.length(); i++){
+                char c = input.charAt(i);
+                if(stack.isEmpty() || c == '('){
+                    stack.push(c);
+                }else if(stack.peek() == '(' && c == ')'){ // vps 일경우 (괄호를 pop
                     stack.pop();
                 }
             }
-            if(stack.isEmpty()){
+
+            if(stack.isEmpty()){ // vps
                 System.out.println("YES");
-            }else {
+            }else{ // !vps
                 System.out.println("NO");
             }
         }
