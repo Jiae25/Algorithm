dd0b29e37c6e6a321e9ae1d26e6c883c21852ee0
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-02-12 15:54:30 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,28 +1,26 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
+
         System.out.println(gcd(a, b));
         System.out.println(lcm(a, b));
     }
-    // 최소공배수(LCM)
+
     private static int lcm(int a, int b) {
         return (a * b) / gcd(a, b);
     }
 
-    // 최대공약수(GCD)
-    private static int gcd(int a, int b) {
+    private static int gcd(int a, int b){
         if(b == 0){
             return a;
         }
         return gcd(b, a % b);
     }
-
 }
