0ff07b3feb3ebb5f0b740344f5658bd2b241f7ae
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-02-13 02:27:30 +0900

    [Bronze II] Title: 소수 찾기, Time: 128 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,28 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int answer = 0;
-        String[] input = br.readLine().split(" ");
-        for (int i = 0; i < input.length; i++){
-            if(isPrime(Integer.parseInt(input[i]))) answer += 1;
-        }
-        System.out.println(answer);
-    }
+        int N = Integer.parseInt(br.readLine()); // 수의 개수
 
-    private static boolean isPrime(int num) {
-        if(num <= 1){
-            return false;
-        }
-        for (int i = 2; i * i <= num; i++){
-            if(num % i == 0) {
-                return false;
+        int MAX = 1000; // 자연수의 범위
+        boolean[] isPrime  = new boolean[MAX + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;  // 0과 1은 false. 소수이면 true
+
+        // Sieve Of Eratosthenes
+        for(int i = 2; i * i <= MAX; i++){
+            if(isPrime[i]){
+                for(int j = i * i; j <= MAX; j+= i){ // i의 배수는 소수가 아님. i*i 미만의 배수는 이미 지워졌음
+                   isPrime[j] = false;
+                }
             }
         }
-        return true;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int answer = 0;
+        for (int i = 0; i < N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(isPrime[num]){ // 주어진 수에서 소수 카운팅
+                answer++;
+            }
+       }
+        System.out.println(answer);
     }
 }
