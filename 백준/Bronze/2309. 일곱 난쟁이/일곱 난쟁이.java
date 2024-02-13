a2f19691d590d34f2077d46bfc76828bce9c8a2b
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-02-13 23:32:36 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 140 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -5,42 +5,35 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int[] nineArr = new int[10]; // 1 ~ 9 인덱스를 사용
-        int nineSum = 0; // 9명 키의 총 합을 저장할 변수
+        int[] dwarfArr = new int[10]; // 1 ~ 9 인덱스를 사용
+        int sum = 0; // 9명 키의 총 합을 저장할 변수
         for(int i = 1; i < 10; i++){ // 배열에 9명의 키 저장
             int height = Integer.parseInt(br.readLine());
-            nineArr[i] = height;
-            nineSum += height; // 9명 키의 합
+            dwarfArr[i] = height;
+            sum += height; // 9명 키의 합
         }
 
-        Arrays.sort(nineArr); // 배열 오름차순 정렬
+        Arrays.sort(dwarfArr); // 배열 오름차순 정렬
 
-        int[] except = findDwarf(nineArr, nineSum); // 뺄 2명 찾기
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < 10; i++){
-            if(i == except[0] || i == except[1]){
-                continue;
-            }else{
-                sb.append(nineArr[i] + "\n");
-            }
-        }
-        System.out.println(sb.delete(sb.length() - 1, sb.length()));
-
-    }
-
-    private static int[] findDwarf(int[] nineArr, int nineSum) {
         for(int i = 1; i < 10; i++){
             for(int j = i + 1; j < 10; j++){
-                int sevenDwarf = nineSum - (nineArr[i] + nineArr[j]);
+                int sevenDwarf = sum - (dwarfArr[i] + dwarfArr[j]);
                 if(sevenDwarf == 100){
-                    return new int[]{i, j};
+                    for(int k = 1; k < 10; k++){
+                        if(k == i || k == j){
+                            continue;
+                        }else{
+                            sb.append(dwarfArr[k] + "\n");
+                        }
+                    }
+                    System.out.println(sb.delete(sb.length() - 1, sb.length()));
+                    return;
                 }else if(sevenDwarf < 100){ // 오름차순으로 정렬해둔 배열이기 때문에 더 계산할 필요가 없음
                     break;
                 }
             }
         }
-        return new int[]{0}; // 일곱난쟁이를 찾을 수 없는 경우는 없다.
     }
 }
