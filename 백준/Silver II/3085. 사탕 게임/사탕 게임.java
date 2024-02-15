f02813863cb29d5ed9c57955bec22a7ba93c27e6
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-02-15 22:22:18 +0900

    [Silver II] Title: 사탕 게임, Time: 176 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver II/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private static int N;
+    private static char[][] board;
+    private static int max = 1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        board = new char[N][N];
+        for(int i = 0; i < N; i++){ // 보드 초기화
+            board[i] = br.readLine().toCharArray();
+        }
+
+        // 가로
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N - 1; j++){
+                // 가로 인접 확인
+                if(board[i][j] == board[i][j+1]){
+                    continue;
+                }else{
+                    swapCandy(i, j, 0);
+                    checkMax(); // 길이체크
+                    swapCandy(i, j, 0); // 원복
+                }
+            }
+        }
+
+        // 세로
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N - 1; j++){
+                if(board[j][i] == board[j+1][i]){ // 세로 인접 확인
+                    continue;
+                }else{
+                    swapCandy(j, i, 1);
+                    checkMax(); // 길이체크
+                    swapCandy(j, i, 1); // 원복
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    private static void view() {
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static void swapCandy(int x, int y, int dir) {
+        if(dir == 0){ // 가로 swap
+            char temp = board[x][y];
+            board[x][y] = board[x][y+1];
+            board[x][y+1] = temp;
+        }else{ // 세로 swap
+            char temp = board[x][y]; // j = x, i = y
+            board[x][y] = board[x+1][y];
+            board[x+1][y] = temp;
+        }
+    }
+
+    private static int checkMax() {
+        // 가로체크
+        for(int i = 0; i < N; i++){
+            int len = 1; // 다음줄 len 초기화
+            for(int j = 0; j < N-1; j++){
+                if(board[i][j] != board[i][j+1]){
+                    len = 1;
+                    continue;
+                }
+                len++;
+                max = Math.max(max, len);
+            }
+        }
+
+        // 세로체크
+        for(int i = 0; i < N; i++){
+            int len = 1;
+            for(int j = 0; j < N-1; j++){
+                if(board[j][i] != board[j+1][i]){
+                    len = 1;
+                    continue;
+                }
+                len++;
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+}
