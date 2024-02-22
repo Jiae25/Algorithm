2cd7379020b34d67e3ae9a12fce4dc809b1efcbe
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-02-22 23:58:30 +0900

    [Silver III] Title: 1, Time: 116 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -3,24 +3,27 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // BufferedReader를 사용하여 입력을 받음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String input;
-        while ((input = br.readLine()) != null && !input.isEmpty()) { // 입력이 null이거나 비어있을 때까지 반복
-            int n = Integer.parseInt(input); // 입력된 문자열을 정수로 변환
-            int num = 0; // 1로만 이루어진 수를 저장할 변수
-            int cnt = 0; // 수를 만들기 위해 1을 더한 횟수를 저장할 변수
-            
-            while(true){ // 1로만 이루어진 수 찾기
-                num = (num * 10 + 1) % n;
-                cnt++;
-
-                if(num == 0){
-                    System.out.println(cnt);
-                    break;
-                }
-
-            }
+        while ((input = br.readLine()) != null && !input.isEmpty()) {
+            int n = Integer.parseInt(input);
+            int result = findMultipleOfOne(n);
+            System.out.println(result);
         }
-        br.close(); // BufferedReader를 닫음
+
+        br.close();
+    }
+
+    private static int findMultipleOfOne(int n) {
+        int num = 1;
+        int count = 1;
+
+        while (num % n != 0) {
+            num = (num * 10 + 1) % n;
+            count++;
+        }
+
+        return count;
     }
 }
