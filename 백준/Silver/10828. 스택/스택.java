2691e09c1e5180b374ba99be1bb4d33246761c89
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-03-02 23:59:18 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 19632 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,43 +2,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 명령의 수
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++){
-            String[] command = br.readLine().split(" ");
-            switch (command[0]){
-                case "push" :
-                    stack.push(Integer.parseInt(command[1]));
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    int num = Integer.parseInt(st.nextToken());
+                    stack.push(num);
                     break;
-                case "pop" :
-                    if(stack.isEmpty()){
-                        System.out.println(-1);
-                    }else{
-                        System.out.println(stack.pop());
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append("-1").append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
                     }
                     break;
-                case "size" :
-                    System.out.println(stack.size());
+                case "size":
+                    sb.append(stack.size()).append("\n");
                     break;
-                case "empty" :
-                    if(stack.isEmpty()){
-                        System.out.println(1);
-                    }else{
-                        System.out.println(0);
+                case "empty":
+                    if (stack.isEmpty()) {
+                        sb.append("1").append("\n");
+                    } else {
+                        sb.append("0").append("\n");
                     }
                     break;
-                case "top" :
-                    if(stack.isEmpty()){
-                        System.out.println(-1);
-                    }else{
-                        System.out.println(stack.peek());
+                case "top":
+                    if (stack.isEmpty()) {
+                        sb.append("-1").append("\n");
+                    } else {
+                        sb.append(stack.peek()).append("\n");
                     }
                     break;
             }
         }
+
+        System.out.println(sb.toString());
+
+        br.close();
     }
 }
