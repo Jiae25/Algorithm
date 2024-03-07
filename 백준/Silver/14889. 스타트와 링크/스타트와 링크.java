20d4685767afb4d43f3c106e5c3c3a8cdfbfc133
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-03-07 22:51:28 +0900

    [Silver I] Title: 스타트와 링크, Time: 324 ms, Memory: 18948 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,67 +1,59 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     private static int N;
-    private static int[][] map;
+    private static int[][] S;
     private static boolean[] visited;
-    private static int min = Integer.MAX_VALUE;
+    private static int minDiff = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 모인 사람의 수
-
-        map = new int[N][N]; // 능력치
-        StringTokenizer st;
-        for(int i = 0; i < N; i++){ // map 초기화
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine()); // 축구를 하기 위해 모인 사람의 수. N은 짝수
+        S = new int[N][N]; // 능력치
+        // i번 사람과 j번 사람이 같은 팀에 속했을 때 팀에 더해지는 능력치
+        for (int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            for (int j = 0; j < N; j++){
+                S[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         visited = new boolean[N];
+        combination(0, 0); // 시작값 인덱스, 선택된 사람수
+        System.out.println(minDiff);
 
-        createTeam(0, 0); // depth
-
-        System.out.println(min);
-
-        br.close();
     }
 
-    private static void createTeam(int start, int depth) { // dfs
-        if(depth == N / 2){
-            addAbility(); // 팀별 능력치의 합을 구하는 메서드
+    private static void combination(int idx, int cnt) {
+        // N/2 로 start 팀과 link 팀으로 나눔
+        if(cnt == N / 2){
+            int start = 0;
+            int link = 0;
+            // S배열에서 능력치 계산
+            // 각 팀의 능력치의 합
+            for (int i = 0; i < N; i++){
+                for (int j = 0; j < N; j++){
+                    if(visited[i] && visited[j]){ // start 팀
+                        start += S[i][j];
+                    }else if(!visited[i] && !visited[j]){ // link 팀
+                        link += S[i][j];
+                    }
+                }
+            }
+            // 팀간의 능력치 차이 계산
+            int diff = Math.abs(start-link);
+            // start팀과 link팀의 최소 능력차이 구하기
+            minDiff = Math.min(diff, minDiff);
             return;
         }
-        for(int i = start; i < N; i++){
-            visited[i] = true;
-            createTeam(i + 1, depth + 1);
-            visited[i] = false;
+        // 선수 팀 나누기
+        for (int i = idx; i < N; i++){
+            visited[i] = true; // 선택받은 선수 방문 처리
+            combination(i+1, cnt+1); // 시작한 수의 다음 수, 선수 추가
+            visited[i] = false; // 방문 처리한 선수 false로 변경
+
         }
-    }
-
-    private static void addAbility() {
-        int start = 0;
-        int link = 0;
-
-        for(int i = 0; i < N; i++){
-            if(visited[i]){
-
-            }
-        }
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                if(visited[i] && visited[j]) {
-                    start += map[i][j];
-                }
-                if(!visited[i] && !visited[j]){
-                    link += map[i][j];
-                }
-            }
-        }
-
-        min = Math.min(min, Math.abs(start - link));
     }
 }
