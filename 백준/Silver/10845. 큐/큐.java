3f3f74c8654018a13abda0c5587139867f536443
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-03-15 01:46:02 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 18964 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,37 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Queue<String> queue = new LinkedList<>();
-        String last = "";
-        for (int i = 0; i < N; i++){
-            String[] input = br.readLine().split(" ");
-            switch (input[0]){
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine()); // 명령의 수
+
+        Queue<Integer> queue = new LinkedList<>();
+        int last = 0;
+        int num = 0;
+        for(int i = 0; i < N; i++){
+            String input = br.readLine();
+
+            if(input.length() > 4 && input.substring(0, 4).equals("push")){ // pop일 때 StringIndexOutOfBoundsException 발생하므로 4자 이상일때만 체크
+                num = Integer.parseInt(input.substring(5));
+                last = num;
+                input = "push";
+            }
+
+            switch (input){
                 case "push" :
-                    queue.add(input[1]);
-                    last = input[1];
+                    queue.add(num);
                     break;
                 case "pop" :
-                    System.out.println(queue.isEmpty() ? -1 : queue.poll());
+                    if(queue.isEmpty()){
+                        sb.append(-1).append("\n");
+                        last = -1;
+                    }else{
+                        sb.append(queue.poll()).append("\n");
+                    }
                     break;
                 case "size" :
-                    System.out.println(queue.size());
+                    sb.append(queue.size()).append("\n");
                     break;
                 case "empty" :
-                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
                     break;
                 case "front" :
-                    System.out.println(queue.isEmpty() ? -1 : queue.peek());
+                    if(queue.isEmpty()){
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(queue.peek()).append("\n");
+                    }
                     break;
                 case "back" :
-                    System.out.println(queue.isEmpty() ? -1 : last);
+                    sb.append(!queue.isEmpty() ? last : -1).append("\n");
                     break;
             }
         }
+        // 결과 출력
+        System.out.println(sb.toString());
+        
+        br.close();
     }
 }
