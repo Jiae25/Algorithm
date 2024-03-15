9154d95ede5decc8c1b34721352c1261abe5ae3f
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-03-15 13:02:50 +0900

    [Silver IV] Title: 덱, Time: 212 ms, Memory: 18884 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,41 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Deque<String> deque = new ArrayDeque<>();
-        for (int i = 0; i < N; i++){
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine()); // 명령의 수
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        int num = 0;
+        for(int i = 0; i < N; i++){
             String[] input = br.readLine().split(" ");
-            switch (input[0]){
+            String command = input[0];
+
+            if(command.contains("push")){
+                num = Integer.parseInt(input[1]);
+            }
+
+            switch (command){
                 case "push_front" :
-                    deque.addFirst(input[1]);
+                    deque.addFirst(num);
                     break;
                 case "push_back" :
-                    deque.addLast(input[1]);
+                    deque.addLast(num);
                     break;
                 case "pop_front" :
-                    System.out.println(deque.isEmpty() ? -1 : deque.pollFirst());
+                    if(deque.isEmpty()){
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(deque.pollFirst()).append("\n");
+                    }
                     break;
                 case "pop_back" :
-                    System.out.println(deque.isEmpty() ? -1 : deque.pollLast());
+                    if(deque.isEmpty()){
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(deque.pollLast()).append("\n");
+                    }
                     break;
                 case "size" :
-                    System.out.println(deque.size());
+                    sb.append(deque.size()).append("\n");
                     break;
                 case "empty" :
-                    System.out.println(deque.isEmpty() ? 1 : 0);
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
                     break;
                 case "front" :
-                    System.out.println(deque.isEmpty() ? -1 : deque.peekFirst());
+                    if(deque.isEmpty()){
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(deque.peekFirst()).append("\n");
+                    }
                     break;
                 case "back" :
-                    System.out.println(deque.isEmpty() ? -1 : deque.peekLast());
+                    if(deque.isEmpty()){
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(deque.peekLast()).append("\n");
+                    }
                     break;
             }
         }
+        // 결과 출력
+        System.out.println(sb.toString());
+        
+        br.close();
     }
 }
