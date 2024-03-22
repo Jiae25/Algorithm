f84099de6c60c5361f43eafb93258cd12d0fe550
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-03-22 23:04:53 +0900

    [Gold V] Title: ABCDE, Time: 292 ms, Memory: 24796 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
     private static ArrayList<Integer>[] graph;
-    private static boolean found;
+    private static boolean answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,36 +14,35 @@ public class Main {
         int N = Integer.parseInt(st.nextToken()); // 사람의 수
         int M = Integer.parseInt(st.nextToken()); // 친구 관계의 수
 
-        graph = new ArrayList[N]; // 그래프 초기화
+        graph = new ArrayList[N]; // 그래프
         for(int i = 0; i < N; i++){
             graph[i] = new ArrayList<>();
         }
-        
+
         for(int i = 0; i < M; i++){ // 간선 정보 입력
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             graph[a].add(b);
-            graph[b].add(a); // 양방향 간선이므로 추가
+            graph[b].add(a); // 양방향
         }
-        
+
         for(int i = 0; i < N; i++){
-            boolean[] visited = new boolean[N]; // 새로운 시작 정점에서 DFS 수행할 때마다 초기화
+            boolean[] visited = new boolean[N];
             visited[i] = true;
-            dfs(i, 1, visited);
-            if(found){
+            dfs(i, 1, visited); // 정점, depth, 방문
+            if(answer){
                 break;
             }
         }
 
-        System.out.println(found ? 1 : 0); // 결과 출력
+        System.out.println(answer ? 1 : 0); // 결과 출력
 
-        br.close();
     }
 
     private static void dfs(int v, int depth, boolean[] visited) {
-        if(depth == 5){ // 경로 길이가 5인 경우
-            found = true;
+        if(depth == 5){
+            answer = true;
             return;
         }
         for(int next : graph[v]){
