75573df5358cdd94b14b7eb46bc342549b1a5d5d
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-03-27 03:28:54 +0900

    [Silver I] Title: 절댓값 힙, Time: 328 ms, Memory: 25616 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,36 +1,40 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> minAbsHeap = new PriorityQueue<Integer>((a, b) -> {
-            int absA = Math.abs(a);
-            int absB = Math.abs(b);
 
-            if(absA == absB){
-                return Integer.compare(a, b);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int abs1 = Math.abs(o1);
+                int abs2 = Math.abs(o2);
+                if(abs1 == abs2){
+                    return o1.compareTo(o2); // 절대값이 같으면 더 작은 수를 우선
+                }
+                return abs1 - abs2; // 절대값이 작은 수를 우선
             }
-
-            return Integer.compare(absA, absB);
         });
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++){
+        for(int i = 0; i < N; i++){
             int x = Integer.parseInt(br.readLine());
             if(x == 0){
-                if(minAbsHeap.isEmpty()){
-                    sb.append("0\n");
+                if(pq.isEmpty()) {
+                    sb.append(0).append("\n");
                 }else{
-                    sb.append(minAbsHeap.poll()).append("\n");
+                    sb.append(pq.poll()).append("\n");
                 }
+
             }else{
-                minAbsHeap.add(x);
+                pq.offer(x);
             }
         }
         System.out.println(sb.toString());
+        br.close();
     }
 }
