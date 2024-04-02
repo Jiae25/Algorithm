6d26285773f938ecc757969163d99dd7f2c0890d
Jiae Ahn <77441385+Jiae25@users.noreply.github.com>
2024-04-02 23:55:32 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // TC
-        for(int t = 0; t < T; t++){
-            Stack<Character> stack = new Stack<>();
-            String input = br.readLine();
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스
 
-            if(input.length() % 2 != 0 || input.charAt(0) == ')'){ // 짝이 맞지 않는 괄호 || )로 시작되는 괄호
-                System.out.println("NO");
-                continue;
-            }
-
-            for(int i = 0; i < input.length(); i++){
-                char c = input.charAt(i);
-                if(stack.isEmpty() || c == '('){
-                    stack.push(c);
-                }else if(stack.peek() == '(' && c == ')'){ // vps 일경우 (괄호를 pop
-                    stack.pop();
-                }
-            }
-
-            if(stack.isEmpty()){ // vps
-                System.out.println("YES");
-            }else{ // !vps
-                System.out.println("NO");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            String str = br.readLine();
+            if (isValid(str)) {
+                sb.append("YES").append('\n');
+            } else {
+                sb.append("NO").append('\n');
             }
         }
+        br.close();
+
+        System.out.println(sb);
+    }
+
+    // 주어진 문자열이 올바른 괄호 문자열인지 판별하는 함수
+    private static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // ')'가 나왔는데 스택이 비어있으면 올바른 괄호 문자열이 아님
+                }
+                stack.pop(); // '('와 ')'가 맞아 떨어짐
+            }
+        }
+        return stack.isEmpty(); // 스택이 비어있으면 올바른 괄호 문자열
     }
 }
